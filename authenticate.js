9b826cb1f23b594f292a71612511343498714123
anju0713 <110585760+anju0713@users.noreply.github.com>
2022-10-16 09:46:19 +0530

    Add files via upload

---
+++ authenticate.js
@@ -0,0 +1,23 @@
+const Users = require('../models/userSchema');
+
+const authenticate = async(req , res) =>{
+    try{
+        const token = req.cookies.jwt;
+        if(!token){
+            res.status(401).send("No token")
+        }else{
+            const verifyToken = jwt.verify(token , process.env.SECRET_KEY);
+            const rootUser = await Users.findOne({_id : verifyToken._id , "tokens.token" : token});
+
+            if(!rootUser){
+                res.status(401).send("User Not Found")
+            }else{
+                res.status(200).send("Authorized User")
+            }
+        }
+        next()
+    }catch(error){
+        res.status(401).send("Error")
+        console.log(error)
+    }
+}
